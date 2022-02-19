a245e19024a8436702da79bdec003c1ef8e7188f
jeon1787 <wjsdmsdn1787@naver.com>
2022-02-19 20:32:14 +0900

    feat: add template project

---
+++ src/main/java/com/kh/reading_fly/ReadingFlyApplication.java
@@ -0,0 +1,13 @@
+package com.kh.reading_fly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingFlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingFlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/reading_fly/ReadingFlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.reading_fly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingFlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
