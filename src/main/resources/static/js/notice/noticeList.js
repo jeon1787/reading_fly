83feecd87d204df36489705f665694189c171b43
hyungeunpark <phg2233@naver.com>
2022-03-12 18:19:11 +0900

    Revert "공지사항 1차구현"
    This reverts commit e069d28b5868ffe74500ad4c7c209a7902cf9b7e.

--- src/main/resources/static/js/notice/noticeList.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-  const $writeBtn = document.getElementById('writeBtn');
-    $writeBtn.addEventListener('click',()=>{
-      const url = `/notices`;
-      location.href = url
-    });
