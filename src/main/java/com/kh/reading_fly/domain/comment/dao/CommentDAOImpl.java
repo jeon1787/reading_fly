d5c18c2a4cbc67c23c34241066f60196d8b9ba6a
jeon1787 <wjsdmsdn1787@naver.com>
2022-03-12 18:27:23 +0900

    게시글 삭제 구현

---
+++ src/main/java/com/kh/reading_fly/domain/comment/dao/CommentDAOImpl.java
@@ -0,0 +1,38 @@
+package com.kh.reading_fly.domain.comment.dao;
+
+import com.kh.reading_fly.domain.comment.dto.CommentDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class CommentDAOImpl implements CommentDAO{
+
+  private final JdbcTemplate jdbcTemplate;
+
+  /**
+   * 전체조회
+   * @return
+   */
+  @Override
+  public List<CommentDTO> selectAll() {
+
+    //sql 작성
+    StringBuffer sql = new StringBuffer();
+    sql.append(" select cnum, ccontent, ccdate, cudate, cid, nickname ");
+    sql.append(" from comments, member ");
+    sql.append(" where comments.cid = member.id ");
+    sql.append(" order by ccdate desc ");
+
+    //sql 실행
+    List<CommentDTO> list = jdbcTemplate.query(sql.toString(), new BeanPropertyRowMapper<>(CommentDTO.class));
+
+    return null;
+  }
+}
---
+++ src/main/java/com/kh/reading_fly/web/form/comment/ApiResult.java
@@ -0,0 +1,7 @@
+package com.kh.reading_fly.web.form.comment;
+
+public class ApiResult<T> {
+  private String rtcd;
+  private String rtmsg;
+  private T data;
+}
