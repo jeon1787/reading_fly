0d466a084fa9ce9f3eebe9812909ffd6f5e840da
hyungeunpark <phg2233@naver.com>
2022-03-13 17:54:42 +0900

    공지사항 이벤트 구현

--- src/main/resources/static/js/notice/noticeAddForm.js
+++ src/main/resources/static/js/notice/noticeAddForm.js
@@ -1,11 +1,41 @@
 'use strict';
 
- const $writeBtn = document.getElementById('writeBtn');
-    const $listBtn = document.getElementById('listBtn');
-    $writeBtn.addEventListener('click', e=>{
-      e.target.closest('form').submit();
-    });
-    $listBtn.addEventListener('click', ()=>{
-      const url = `/notices/all`;
-      location.href = url;
-    });
+  const $writeBtn = document.getElementById('writeBtn');
+  const $listBtn = document.getElementById('listBtn');
+  const $nTitle = document.getElementById('nTitle');
+  const $nContent = document.getElementById('nContent');
+  $writeBtn.addEventListener('click', e=>{
+
+    if($nTitle.value == "") {
+      alert('제목을 입력해주세요');
+      return;
+    }
+
+    if($nContent.value == "") {
+      alert('내용을 입력해주세요');
+      return;
+    }
+
+    if($nTitle.value.length > 20) {
+      alert('글자수 제한을 초과해서 입력 불가');
+      return;
+    }
+
+    if($nContent.value.length > 1000) {
+      alert($nContent.value.length +'글자수 제한을 초과해서 입력 불가');
+      return;
+    }
+
+    e.target.closest('form').submit();
+   });
+
+  $listBtn.addEventListener('click', ()=>{
+    const url = `/notices/all`;
+    location.href = url;
+  });
+
+ $cancelBtn.addEventListener('click', e=>{
+  if(!confirm('변경사항이 저장되지 않을 수 있습니다')) return;
+    const url = `/notices/all`;
+    location.href = url;
+  });
