f934e5cf7b7b7d200d4eecebbe32a768989e374f
hyungeunpark <phg2233@naver.com>
2022-03-19 16:56:49 +0900

    Q&A 기능 1차 구현

---
+++ src/main/java/com/kh/reading_fly/domain/qna/dao/QnaDAOImpl.java
@@ -0,0 +1,254 @@
+package com.kh.reading_fly.domain.qna.dao;
+
+import com.kh.reading_fly.domain.qna.dto.QnaDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class QnaDAOImpl implements QnaDAO {
+
+  private  final JdbcTemplate jdbcTemplate;
+
+
+  //원글작성
+  @Override
+  public Long saveOrigin(QnaDTO qna) {
+
+    //SQL작성
+    StringBuffer sql = new StringBuffer();
+//    sql.append("insert into qna (qnum,qtitle,qemail,qnickname,qcontent,qgroup) ");
+    sql.append("insert into qna (qnum,qtitle,qnickname,qcontent,qgroup) ");
+//    sql.append("values(qna_qnum_seq.nextval,?,?,?,?,qna_qnum_seq.currval) ");
+    sql.append("values(qna_qnum_seq.nextval,?,?,?,qna_qnum_seq.currval) ");
+
+    //SQL실행
+    KeyHolder keyHolder = new GeneratedKeyHolder();
+    jdbcTemplate.update(new PreparedStatementCreator() {
+      @Override
+      public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+        PreparedStatement pstmt = con.prepareStatement(
+            sql.toString(),
+            new String[]{"qnum"}  // insert 후 insert 레코드중 반환할 컬럼명, KeyHolder에 저장됨.
+        );
+
+        pstmt.setString(1, qna.getQTitle());
+        pstmt.setString(2, qna.getQNickname());
+        pstmt.setString(3, qna.getQContent());
+
+        return pstmt;
+      }
+    },keyHolder);
+
+    return Long.valueOf(keyHolder.getKeys().get("qnum").toString());
+  }
+
+  //목록
+  @Override
+  public List<QnaDTO> findAll() {
+    StringBuffer sql = new StringBuffer();
+    sql.append("SELECT " );
+    sql.append(" qnum," );
+    sql.append(" qtitle, " );
+//    sql.append(" qemail, " );
+    sql.append(" qnickname, " );
+    sql.append(" qhit, " );
+    sql.append(" qcontent, " );
+    sql.append(" pqnum, " );
+    sql.append(" qgroup, " );
+    sql.append(" qstep, " );
+    sql.append(" qindent, " );
+    sql.append(" qstatus, " );
+    sql.append(" qcdate, " );
+    sql.append(" qudate " ) ;
+    sql.append("FROM ");
+    sql.append(" qna ");
+    sql.append("Order by qgroup desc, qstep asc ");
+
+    List<QnaDTO> list = jdbcTemplate.query(
+        sql.toString(), new BeanPropertyRowMapper<>(QnaDTO.class));
+
+    return list;
+  }
+
+  //조회
+  @Override
+  public QnaDTO findByQNum(Long qNum) {
+    StringBuffer sql = new StringBuffer();
+    sql.append("SELECT " );
+    sql.append(" qnum," );
+    sql.append(" qtitle, " );
+//    sql.append(" qemail, " );
+    sql.append(" qnickname, " );
+    sql.append(" qhit, " );
+    sql.append(" qcontent, " );
+    sql.append(" pqnum, " );
+    sql.append(" qgroup, " );
+    sql.append(" qstep, " );
+    sql.append(" qindent, " );
+    sql.append(" qstatus, " );
+    sql.append(" qcdate, " );
+    sql.append(" qudate " ) ;
+    sql.append("FROM ");
+    sql.append(" qna ");
+    sql.append("where qnum = ? ");
+
+    QnaDTO qnaItem = null;
+    try {
+      qnaItem = jdbcTemplate.queryForObject(
+          sql.toString(),
+          new BeanPropertyRowMapper<>(QnaDTO.class),
+          qNum);
+    }catch (Exception e) {  // 1건을 못찾으면
+      qnaItem = null;
+    }
+
+    return qnaItem;
+  }
+
+  //삭제
+  @Override
+  public int deleteByQNum(Long qNum) {
+    StringBuffer sql = new StringBuffer();
+    sql.append("DELETE FROM qna ");
+    sql.append(" WHERE qnum = ? ");
+
+    int updateItemCount = jdbcTemplate.update(sql.toString(), qNum);
+
+    return updateItemCount;
+  }
+
+  //수정
+  @Override
+  public int updateByQNum(Long qNum, QnaDTO qna) {
+    StringBuffer sql = new StringBuffer();
+    sql.append("UPDATE qna" );
+    sql.append("  SET qtitle = ?," );
+    sql.append("    qcontent = ?," );
+    sql.append("    qudate = systimestamp" );
+    sql.append(" WHERE qnum = ?" );
+
+    int updatedItemCount = jdbcTemplate.update(
+        sql.toString(),
+        qna.getQTitle(),
+        qna.getQContent(),
+        qNum
+    );
+
+    return updatedItemCount;
+  }
+
+  //답글
+  @Override
+  public Long saveReply(Long pQNum, QnaDTO replyQna) {
+
+    //부모글 참조반영
+    QnaDTO qna = addInfoOfParentToChild(pQNum,replyQna );
+
+    StringBuffer sql = new StringBuffer();
+//    sql.append("insert into qna (qnum,qtitle,qemail,qnickname,qcontent,pqnum,qgroup,qstep,qindent) ");
+    sql.append("insert into qna (qnum,qtitle,qnickname,qcontent,pqnum,qgroup,qstep,qindent) ");
+//    sql.append("values(qna_qnum_seq.nextval, ?, ? ,?,?,?,?,?,?) ");
+    sql.append("values(qna_qnum_seq.nextval, ?, ? ,?,?,?,?,?) ");
+
+    KeyHolder keyHolder = new GeneratedKeyHolder();
+    jdbcTemplate.update(new PreparedStatementCreator() {
+      @Override
+      public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+        PreparedStatement pstmt = con.prepareStatement(
+            sql.toString(),
+            new String[]{"qnum"}  // insert 후 insert 레코드중 반환할 컬럼명, KeyHolder에 저장됨.
+        );
+
+        pstmt.setString(1, qna.getQTitle());
+//        pstmt.setString(2, qna.getQEmail());
+        pstmt.setString(2, qna.getQNickname());
+        pstmt.setString(3, qna.getQContent());
+        pstmt.setLong(4,qna.getPQNum());
+        pstmt.setLong(5,qna.getQGroup());
+        pstmt.setInt(6,qna.getQStep());
+        pstmt.setInt(7,qna.getQIndent());
+        return pstmt;
+      }
+    },keyHolder);
+
+    return Long.valueOf(keyHolder.getKeys().get("qnum").toString());
+  }
+
+  //답글에 부모정보 반영하기
+  private QnaDTO addInfoOfParentToChild(Long pQNum, QnaDTO replyQna) {
+    //부모글
+    QnaDTO qna = findByQNum(pQNum);
+
+    //bgroup
+    //답글의 bgroup = 부모글의 bgroup
+    replyQna.setQGroup(qna.getQGroup());
+
+    //step 로직
+    //1) 부모글의 bgroup값과 동일한 게시글중 부모글의 step보다큰 게시글의 step을 1씩 증가
+    int affectedRows = updateStep(qna);
+
+    //2) 답글의 step값은 부모글의 step값 +1
+    replyQna.setQStep(qna.getQStep()+1);
+
+    //bindent 로직
+    // 답글의 bindent = 부모글의 bindent +1
+    replyQna.setQIndent(qna.getQIndent()+1);
+
+    replyQna.setPQNum(pQNum);
+    return  replyQna;
+  }
+
+  //부모글과 동일한그룹 step반영
+  private int updateStep(QnaDTO qna) {
+    StringBuffer sql = new StringBuffer();
+
+    sql.append(" update qna ");
+    sql.append(" set qstep = qstep + 1 ");
+    sql.append(" where qgroup =  ? ");
+    sql.append(" and qstep  >  ? ");
+
+    int affectedRows = jdbcTemplate.update(sql.toString(), qna.getQGroup(), qna.getQStep());
+
+    return affectedRows;
+
+  }
+
+  //조회수증가
+  @Override
+  public int increaseHitCount(Long qNum) {
+    StringBuffer sql = new StringBuffer();
+    sql.append("update qna ");
+    sql.append("   set qhit = qhit + 1 ");
+    sql.append(" where qnum = ? ");
+
+    int affectedRows = jdbcTemplate.update(sql.toString(), qNum);
+
+    return affectedRows;
+  }
+
+  //전체건수
+  @Override
+  public int totalCount() {
+
+    String sql = "select count(*) from qna";
+
+    Integer cnt = jdbcTemplate.queryForObject(sql, Integer.class);
+
+    return cnt;
+  }
+
+
+}
---
+++ src/main/java/com/kh/reading_fly/domain/qna/svc/QnaSVC.java
@@ -0,0 +1,63 @@
+package com.kh.reading_fly.domain.qna.svc;
+
+import com.kh.reading_fly.domain.qna.dto.QnaDTO;
+
+import java.util.List;
+
+public interface QnaSVC {
+  /**
+   * 원글작성
+   *
+   * @param qna
+   * @return 게시글 번호
+   */
+  Long saveOrigin(QnaDTO qna);
+
+  /**
+   * 목록
+   *
+   * @return
+   */
+  List<QnaDTO> findAll();
+
+  /**
+   * 상세 조회
+   *
+   * @param qNum 게시글번호
+   * @return
+   */
+  QnaDTO findByQNum(Long qNum);
+
+  /**
+   * 삭제
+   *
+   * @param qNum 게시글번호
+   * @return 삭제건수
+   */
+  int deleteByQNum(Long qNum);
+
+  /**
+   * 수정
+   *
+   * @param qNum  게시글 번호
+   * @param qna 수정내용
+   * @return 수정건수
+   */
+  int updateByQNum(Long qNum, QnaDTO qna);
+
+  /**
+   * 답글작성
+   *
+   * @param pQNum   부모글번호
+   * @param replyQna 답글
+   * @return 답글번호
+   */
+  Long saveReply(Long pQNum, QnaDTO replyQna);
+
+  /**
+   * 전체건수
+   *
+   * @return 게시글 전체건수
+   */
+  int totalCount();
+}
