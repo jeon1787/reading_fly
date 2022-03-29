9e41d427aa40531a0df290e017bb3d77344c77a3
hyungeunpark <phg2233@naver.com>
2022-03-29 17:54:30 +0900

    1. 공지사항 화면 수정 2. 등록 수정 시 제목 내용 미입력 할 때 알려주고 사이즈를 넘겨도 알려줌

--- src/main/resources/static/js/notice/noticeAddForm.js
+++ src/main/resources/static/js/notice/noticeAddForm.js
@@ -1,43 +1,12 @@
 'use strict';
-
-  const $writeBtn = document.getElementById('writeBtn');
-
-  const $cancelBtn = document.getElementById('cancelBtn');
-  const $listBtn = document.getElementById('listBtn');
-  const $nTitle = document.getElementById('nTitle');
-  const $nContent = document.getElementById('nContent');
-  $writeBtn.addEventListener('click', e=>{
-
-    if($nTitle.value == "") {
-      alert('제목을 입력해주세요');
-      return;
-    }
-
-    if($nContent.value == "") {
-      alert('내용을 입력해주세요');
-      return;
-    }
-
-    if($nTitle.value.length > 20) {
-      alert('글자수 제한을 초과해서 입력 불가');
-      return;
-    }
-
-    if($nContent.value.length > 1000) {
-      alert('글자수 제한을 초과해서 입력 불가');
-      return;
-    }
-
-    e.target.closest('form').submit();
-   });
-
-  $listBtn.addEventListener('click', ()=>{
-    const url = `/notices/all`;
-    location.href = url;
-  });
-
- $cancelBtn.addEventListener('click', e=>{
-  if(!confirm('변경사항이 저장되지 않을 수 있습니다')) return;
-    const url = `/notices/all`;
-    location.href = url;
-  });
+//등록
+const $writeBtn = document.getElementById('writeBtn');
+$writeBtn?.addEventListener("click", e=>{
+  writeForm.submit();
+});
+//목록
+const $listBtn = document.getElementById('listBtn');
+$listBtn?.addEventListener("click",e=>{
+        const url = `/notices/all`;
+       location.href = url;
+});
