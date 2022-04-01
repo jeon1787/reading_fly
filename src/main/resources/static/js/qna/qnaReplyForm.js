261ef4b0fc3fa31e9b70a6d3ba82dcdc54d6b65d
hyungeunpark <phg2233@naver.com>
2022-04-01 12:10:29 +0900

    공지사항 Q&A ckeditor추가

--- src/main/resources/static/js/qna/qnaReplyForm.js
+++ src/main/resources/static/js/qna/qnaReplyForm.js
@@ -1,4 +1,69 @@
 'use strict';
+
+//textArea => ck_editor 대체
+ClassicEditor
+      .create( document.querySelector( '#qContent' ), {
+       plugin:['ListStyle','Markdown','MediaEmbed','MediaEmbedToolbar'],
+         toolbar: {
+            items: [
+               'heading',
+               '|',
+               'underline',
+               'bold',
+               'italic',
+               'link',
+               'bulletedList',
+               'numberedList',
+               'todoList',
+               '|',
+               'outdent',
+               'indent',
+               '|',
+               'imageInsert',
+               'imageUpload',
+               'blockQuote',
+               'insertTable',
+               'mediaEmbed',
+               'markdown',
+               'undo',
+               'redo',
+               '|',
+               'highlight',
+               'fontFamily',
+               'fontColor',
+               'fontBackgroundColor',
+               'fontSize',
+               '|',
+               'htmlEmbed',
+               'specialCharacters'
+            ]
+         },
+         language: 'en',
+         image: {
+            toolbar: [
+               'imageTextAlternative',
+               'imageStyle:full',
+               'imageStyle:side'
+            ]
+         },
+         table: {
+            contentToolbar: [
+               'tableColumn',
+               'tableRow',
+               'mergeTableCells',
+               'tableCellProperties',
+               'tableProperties'
+            ]
+         },
+      })
+      .then( editor => {
+
+         window.editor = editor;
+      } )
+      .catch( error => {
+         console.error( error );
+      } );
+
 //등록
 writeBtn?.addEventListener("click", e=>{
   writeForm.submit();
