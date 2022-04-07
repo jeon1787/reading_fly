aa5e38cf5b48f5af7948ef2ceaf3b5d99bc019bd
hyungeunpark <phg2233@naver.com>
2022-04-07 16:34:00 +0900

    1. 답글 버튼 이벤트 처리 2. 답글 화면 수정

--- src/main/resources/static/js/qna/qnaReplyForm.js
+++ src/main/resources/static/js/qna/qnaReplyForm.js
@@ -64,11 +64,16 @@ ClassicEditor
          console.error( error );
       } );
 
+const $qna = document.querySelector('.wrap');
+const page = $qna.dataset.page;
+
 //등록
 writeBtn?.addEventListener("click", e=>{
   writeForm.submit();
 });
 //목록
-listBtn?.addEventListener("click",e=>{
-  location.href = "/qna/list";
+cancelBtn?.addEventListener("click",e=>{
+   if(!confirm('사이트에서 나가시겠습니까? 변경사항이 저장되지 않을 수 있습니다')) return;
+     const url = `/qna/list/${page}`;
+     location.href = url;
 });
