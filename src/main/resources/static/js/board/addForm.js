f07b28b497e3337b660ad18f2f93326379bc9ce9
jeon1787 <wjsdmsdn1787@naver.com>
2022-04-08 01:53:00 +0900

    게시판 목록 페이지 css 적용
    1. 게시판 목록 페이지 css 적용

--- src/main/resources/static/js/board/addForm.js
+++ src/main/resources/static/js/board/addForm.js
@@ -62,7 +62,23 @@ ClassicEditor
 			console.error( error );
 		} );
 
-//등록 버튼
+//이벤트 막기(Capturing)
+document.body.addEventListener('click', e=>{
+  if(e.target.id == "addBtn") return;
+  if(e.target.tagName == "A" || e.target.tagName == "BUTTON"){
+    if(!confirm("작성을 종료하시겠습니까?")){
+      //동일한 DOM 에 걸린 이벤트를 막기(a 태그)
+      if (e.preventDefault) e.preventDefault();
+      else e.returnValue = false;//IE 대응
+
+      //동일한 DOM 에 걸린 이벤트를 막기(button 태그)
+      if (e.stopImmediatePropagation) e.stopImmediatePropagation();
+      else e.isImmediatePropagationEnabled = false;//IE 대응
+    }
+  }
+}, true)//이벤트 캡쳐링(Capturing)
+
+//등록 버튼(Bubbling)
 addBtn.addEventListener('click', e=>{
   if(document.querySelector('.title').value.trim() == ''){
     alert("제목을 입력하세요.");
@@ -76,4 +92,10 @@ addBtn.addEventListener('click', e=>{
   const url = "/board/add";
   $formTag.action = url;
   $formTag.submit();
+})
+
+//목록 버튼(Bubbling)
+listBtn.addEventListener('click', e=>{
+  const url = "/board";
+  location.href = url;
 })
--- src/main/resources/static/js/board/listForm.js
+++ src/main/resources/static/js/board/listForm.js
@@ -28,3 +28,37 @@ function search_f(e){
     const url = `/board/1/${$searchType.value}/${$keyword.value}`;
     location.href = url;
 }
+
+//페이징 버튼 style 부여
+document.addEventListener('DOMContentLoaded', e=>{
+    console.log('DOM 생성');
+
+    //현재 url에서 'board' 문자열 위치 찾기
+    const urlStr = window.location.href;
+    const location = urlStr.indexOf('board');
+    console.log(location);
+
+    //'board' 뒤 페이지 숫자 파라미터 가져오기
+    let targetPage = '';
+    targetPage += urlStr.charAt(location + 6);
+    targetPage += urlStr.charAt(location + 7);
+    targetPage += urlStr.charAt(location + 8);
+
+    //페이지 파라미터가 없는 경우 1페이지
+    if(targetPage == ''){
+      targetPage = '1';
+    }
+    console.log(targetPage);
+
+    //페이지 버튼 요소 찾기
+    const pageNum = document.getElementsByClassName('board-page-num');
+
+    for( let i = 0; i < pageNum.length; i++ ){
+      let ele = pageNum.item(i);
+
+      //페이지 버튼의 텍스트가 현재 페이지와 일치시 색상 부여
+      if(ele.textContent == targetPage){
+        ele.style.backgroundColor = 'skyblue';
+      }
+    }
+});
