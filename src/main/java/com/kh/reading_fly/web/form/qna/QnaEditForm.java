4f8bbf68af256c6f59e5a009e5d56d32323edb06
hyungeunpark <phg2233@naver.com>
2022-04-09 12:42:28 +0900

    1. 에러 메세지 수정 2. 리스트 class th추가 css적용

--- src/main/java/com/kh/reading_fly/web/form/qna/QnaEditForm.java
+++ src/main/java/com/kh/reading_fly/web/form/qna/QnaEditForm.java
@@ -11,11 +11,10 @@ import java.util.List;
 public class QnaEditForm {
   private Long qNum;             //게시글 번호
   @NotBlank(message = "제목을 입력해주세요")
-  @Size(min=1, max = 20, message = "글자수 20자를 초과할 수 없습니다.")
+  @Size(max = 20, message = "글자수 20자를 초과할 수 없습니다.")
   private String qTitle;         //제목
   private String qNickname;      //별칭
   @NotBlank(message = "내용을 입력해주세요")
-  @Size(min=1, message = "내용은 글자수 최소 1자 입니다.")
   private String qContent;      //내용
   private int qHit;             //조회수
 
--- src/main/java/com/kh/reading_fly/web/form/qna/QnaReplyForm.java
+++ src/main/java/com/kh/reading_fly/web/form/qna/QnaReplyForm.java
@@ -10,13 +10,10 @@ import java.util.List;
 @Data
 public class QnaReplyForm {
   @NotBlank(message = "제목을 입력해주세요")
-  @Size(min=1, max = 20, message = "글자수 20자를 초과할 수 없습니다.")
+  @Size(max = 20, message = "글자수 20자를 초과할 수 없습니다.")
   private String qTitle;         //  제목
-  @NotBlank
-  @Size(min=3,max=15)
   private String qNickname;      //  별칭
   @NotBlank(message = "내용을 입력해주세요")
-  @Size(min=1, message = "내용은 글자수 최소 1자 입니다.")
   private String qContent;      //  내용
 
   private List<MultipartFile> files;  // 첨부파일
